c431eee97b940b3c7561fe825d709aa2024decbc
MrFlexi <Szaroletta@hotmail.com>
2019-02-10 18:21:27 +0100

    Init 2

---
+++ TrainMobilnet_Transfer.py
@@ -0,0 +1,127 @@
+
+import pandas as pd
+import numpy as np
+import os
+import keras
+import matplotlib.pyplot as plt
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNet
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.optimizers import Adam
+
+train_dir = './lego_fotos/train'
+validation_dir = './lego_fotos/validation'
+image_size = 224
+
+
+base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+x=Dense(1024,activation='relu')(x) #dense layer 2
+x=Dense(512,activation='relu')(x) #dense layer 3
+preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
+
+model=Model(inputs=base_model.input,outputs=preds)
+#specify the inputs
+#specify the outputs
+#now a model has been created based on our architecture
+
+for layer in model.layers[:20]:
+    layer.trainable=False
+for layer in model.layers[20:]:
+    layer.trainable=True
+
+train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
+
+train_generator=train_datagen.flow_from_directory('./lego_fotos/train/', # this is where you specify the path to the main data folder
+                                                 target_size=(224,224),
+                                                 color_mode='rgb',
+                                                 batch_size=32,
+                                                 class_mode='categorical',
+                                                 shuffle=True)
+
+
+model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+step_size_train=train_generator.n//train_generator.batch_size
+history = model.fit_generator(generator=train_generator,
+                   steps_per_epoch=step_size_train,
+                    epochs=5)
+
+
+
+
+# Save the Model
+model.save('LegoTrainedMobilenet.h5')
+
+# Plot the accuracy and loss curves
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+# Create a generator for prediction
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(image_size, image_size),
+    batch_size=val_batchsize,
+    class_mode='categorical',
+    shuffle=False)
+
+# Get the filenames from the generator
+fnames = validation_generator.filenames
+
+# Get the ground truth from generator
+ground_truth = validation_generator.classes
+
+# Get the label to class mapping from the generator
+label2index = validation_generator.class_indices
+
+# Getting the mapping from class index to class label
+idx2label = dict((v, k) for k, v in label2index.items())
+
+# Get the predictions from the model using the generator
+predictions = model.predict_generator(validation_generator,
+                                      steps=validation_generator.samples / validation_generator.batch_size, verbose=1)
+predicted_classes = np.argmax(predictions, axis=1)
+
+errors = np.where(predicted_classes != ground_truth)[0]
+print("No of errors = {}/{}".format(len(errors), validation_generator.samples))
+
+# Show the errors
+for i in range(len(errors)):
+    pred_class = np.argmax(predictions[errors[i]])
+    pred_label = idx2label[pred_class]
+
+    title = 'Original label:{}, Prediction :{}, confidence : {:.3f}'.format(
+        fnames[errors[i]].split('/')[0],
+        pred_label,
+        predictions[errors[i]][pred_class])
+
+    original = load_img('{}/{}'.format(validation_dir, fnames[errors[i]]))
+    plt.figure(figsize=[7, 7])
+    plt.axis('off')
+    plt.title(title)
+    plt.imshow(original)
+    plt.show()
