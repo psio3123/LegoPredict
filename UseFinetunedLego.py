0ae93d838f8578d8cf9f51e4935e78d6dd8ab3c8
MrFlexi <Szaroletta@hotmail.com>
2019-02-14 15:53:37 +0100

    Init 2

--- UseFinetunedLego.py
+++ UseFinetunedLego.py
@@ -19,14 +19,6 @@ inception_model = inception_v3.InceptionV3(weights="imagenet")
 
 
 def read_model():
-    start = time.time()
-    print("reading model...")
-    model = load_model('./models/LegoTrainedMobilenet.h5')
-    # model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    # print(model.summary())
-    ende = time.time()
-    print('{:5.3f}s'.format(ende - start))
-    return model
 
 
 def show_results():
@@ -48,9 +40,9 @@ def show_results():
         plt.show()
 
 
-def get_pil_image(filename):
+def get_pil_image(filename, target_size):
     # load an image in PIL format
-    original_image = load_img(filename, target_size=(224, 224))
+    original_image = load_img(filename, target_size=(target_size,target_size))
 
     # convert the PIL image (width, height) to a NumPy array (height, width, channel)
     numpy_image = img_to_array(original_image)
@@ -58,9 +50,9 @@ def get_pil_image(filename):
     # Convert the image into 4D Tensor (samples, height, width, channels) by adding an extra dimension to the axis 0.
     input_image = np.expand_dims(numpy_image, axis=0)
 
-    print("PIL image size = ", original_image.size)
-    print("NumPy image size = ", numpy_image.shape)
-    print("Input image size = ", input_image.shape)
+    #print("PIL image size = ", original_image.size)
+    #print("NumPy image size = ", numpy_image.shape)
+    #print("Input image size = ", input_image.shape)
     plt.imshow(np.uint8(input_image[0]))
     return input_image
 
@@ -88,7 +80,39 @@ def predict_mymodel(img_path):
     img_tensor /= 255.  # imshow expects values in the range [0, 1]
 
     start = time.time()
-    predictions_my = my_model.predict(img_tensor)
+    predictions_my = mobilenet_model.predict(img_tensor)
+    print(predictions_my)
+    # label_MY = decode_predictions(predictions_my)
+    # print("My labels = ", label_MY )
+    ende = time.time()
+    print('{:5.3f}s'.format(ende - start))
+    y_classes = predictions_my.argmax(axis=-1)
+    print('Best Match', y_classes)
+
+def predict_vgg16(img_path):
+    img = image.load_img(img_path, target_size=(244, 244))
+    img_tensor = image.img_to_array(img)  # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor,axis=0)  # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.  # imshow expects values in the range [0, 1]
+
+    start = time.time()
+    predictions_my = vgg16_model.predict(img_tensor)
+    print(predictions_my)
+    # label_MY = decode_predictions(predictions_my)
+    # print("My labels = ", label_MY )
+    ende = time.time()
+    print('{:5.3f}s'.format(ende - start))
+    y_classes = predictions_my.argmax(axis=-1)
+    print('Best Match', y_classes)
+
+def predict_resnet50(img_path):
+    img = image.load_img(img_path, target_size=(244, 244))
+    img_tensor = image.img_to_array(img)  # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor,axis=0)  # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.  # imshow expects values in the range [0, 1]
+
+    start = time.time()
+    predictions_my = resnet50_model.predict(img_tensor)
     print(predictions_my)
     # label_MY = decode_predictions(predictions_my)
     # print("My labels = ", label_MY )
@@ -98,26 +122,7 @@ def predict_mymodel(img_path):
     print('Best Match', y_classes)
 
 
-if __name__ == '__main__':
-
-    my_model = read_model()
-
-    file_3x5LGreen = "./lego_fotos/validation/3x5LGreen/frame20190204-164214788.jpg"
-
-    file_1x4LRed = "./lego_fotos/validation/1x4LRed/frame20190204-16420410.jpg"
-
-    file_rose = "./lego_fotos/predict/rose.jpg"
-    file_sunflower = "./lego_fotos/predict/sunflower.jpg"
-
-    file_car = "./lego_fotos/predict/cat+people.jpg"
-
-    file_apple = "./lego_fotos/predict/apple.jpg"
-
-    file_daisy = "./lego_fotos/train/daisy/1031799732_e7f4008c03.jpg"
-    # ---------------------------------------------------------------------------------------------
-    # Predict SingleImage via Inception V3
-    # ---------------------------------------------------------------------------------------------
-
+def inception():
     # print("Inception - Lego 1x4LRed ")
     # predict_inception_v3( file_1x4LRed )
     print("")
@@ -133,8 +138,9 @@ if __name__ == '__main__':
     print("Inception - Sunflower")
     predict_inception_v3( file_sunflower)
 
+def mobilenet():
     # ---------------------------------------------------------------------------------------------
-    # My Trained Model - Single Image
+    # My Trained Model MobileNet- Single Image
     # ---------------------------------------------------------------------------------------------
     print("")
     print("My Model - 1x5LGreen ")
@@ -148,8 +154,44 @@ if __name__ == '__main__':
     print("")
     print("")
     print("Inception - Apple ")
-    predict_inception_v3(file_apple)
+    predict_mymodel(file_apple)
     print("")
     print("Inception - Sunflower")
-    predict_inception_v3(file_sunflower)
+    predict_mymodel(file_sunflower)
+
+def resnet50():
+    # ---------------------------------------------------------------------------------------------
+    # My Trained Model MobileNet- Single Image
+    # ---------------------------------------------------------------------------------------------
+    print("")
+    print("Resnet50 - 1x5LGreen ")
+    predict_resnet50(file_3x5LGreen)
+
+if __name__ == '__main__':
+    start = time.time()
+    print("reading model...")
+
+    mobilenet_model = load_model('./models/LegoTrainedMobilenet.h5')
+    #vgg16_model = load_model('./models/LegoTrainedVGG16_epochs10.h5')
+    resnet50_model = load_model('./models/resnet50_lego_01.h5')
+
+    ende = time.time()
+    print('{:5.3f}s'.format(ende - start))
+
+    file_3x5LGreen = "./lego_fotos/validation/3x5LGreen/frame20190204-164214788.jpg"
+
+    file_1x4LRed = "./lego_fotos/validation/1x4LRed/frame20190204-16420410.jpg"
+
+    file_rose = "./lego_fotos/predict/rose.jpg"
+    file_sunflower = "./lego_fotos/predict/sunflower.jpg"
+
+    file_car = "./lego_fotos/predict/cat+people.jpg"
+
+    file_apple = "./lego_fotos/predict/apple.jpg"
+
+    file_daisy = "./lego_fotos/train/daisy/1031799732_e7f4008c03.jpg"
+
+    resnet50()
+
+
 
